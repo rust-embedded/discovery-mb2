6906b92f73c0aca565c658bfc1a7878c65b82296
Bart Massey <bart.massey@gmail.com>
2024-07-09 15:01:51 -0700

    removed unsafe code in serial-setup
    Thanks much to James Munns for the advice on how to do this.

--- mdbook/src/serial-setup/src/lib.rs
+++ mdbook/src/serial-setup/src/lib.rs
@@ -4,20 +4,14 @@ use core::fmt;
 use embedded_io::{Read, Write};
 use microbit::hal::uarte::{self, Instance, Uarte, UarteRx, UarteTx};
 
-static mut TX_BUF: [u8; 1] = [0; 1];
-static mut RX_BUF: [u8; 1] = [0; 1];
-
 #[allow(unused)]
 pub struct UartePort<T: Instance>(UarteTx<T>, UarteRx<T>);
 
 impl<T: Instance> UartePort<T> {
     pub fn new(serial: Uarte<T>) -> UartePort<T> {
-        // XXX Fix me: Need to use `split()` in a safe way if possible.
-        // This may require an API change in the `nrf-hal` crate.
-        #[allow(static_mut_refs)]
-        let (tx, rx) = serial
-            .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
-            .unwrap();
+        let tx_buf = cortex_m::singleton!(TX_BUF: [u8; 1] = [0u8; 1]).unwrap();
+        let rx_buf = cortex_m::singleton!(RX_BUF: [u8; 1] = [0u8; 1]).unwrap();
+        let (tx, rx) = serial.split(tx_buf, rx_buf).unwrap();
         UartePort(tx, rx)
     }
 }
