4284f0907090ae66438a8b33789413b8ece2c27d
Bart Massey <bart.massey@gmail.com>
2024-10-26 15:39:53 -0700

    cleaned up pokier.rs

--- mdbook/src/14-interrupts/examples/pokier.rs
+++ mdbook/src/14-interrupts/examples/pokier.rs
@@ -1,11 +1,9 @@
 #![no_main]
 #![no_std]
 
-use core::cell::RefCell;
-
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use critical_section::Mutex;
+use critical_section_lock_mut::LockMut;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -17,17 +15,12 @@ use microbit::{
     },
 };
 
-static TE: Mutex<RefCell<Option<gpiote::Gpiote>>> =
-    Mutex::new(RefCell::new(None));
+static TE: LockMut<gpiote::Gpiote> = LockMut::new();
 
 #[interrupt]
 fn GPIOTE() {
     rprintln!("ouch");
-    critical_section::with(|cs| {
-        let cell = TE.borrow(cs).borrow();
-        let channel = cell.as_ref().unwrap().channel0();
-        channel.reset_events();
-    });
+    TE.with_lock(|te| te.channel0().reset_events());
 }
 
 #[entry]
@@ -35,21 +28,24 @@ fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
     let button_a = board.buttons.button_a.into_floating_input();
+
+    // Set up the GPIOTE to generate an interrupt when Button A is pressed (GPIO
+    // wire goes low).
     let gpiote = gpiote::Gpiote::new(board.GPIOTE);
     let channel = gpiote.channel0();
     channel
         .input_pin(&button_a.degrade())
-        .lo_to_hi()
+        .hi_to_lo()
         .enable_interrupt();
     channel.reset_events();
-    
-    critical_section::with(|cs| {
-        let mut cell = TE.borrow(cs).borrow_mut();
-        *cell = Some(gpiote);
-    });
+    TE.init(gpiote);
+
+    // Set up the NVIC to handle GPIO interrupts.
     unsafe { pac::NVIC::unmask(pac::Interrupt::GPIOTE) };
     pac::NVIC::unpend(pac::Interrupt::GPIOTE);
+
     loop {
+        // "wait for interrupt": CPU goes to sleep until an interrupt.
         asm::wfi();
         rprintln!("got poked");
     }
