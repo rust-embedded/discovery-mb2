19d7ed4dd0bb71f4b27b8bea43162993fb048007
BartMassey <BartMassey@users.noreply.github.com>
2024-11-08 19:20:49 +0000

    deploy: 7376bbd948695226b50a61838ee3f30d12bb949e

---
+++ 07-registers/src/lib.rs
@@ -0,0 +1,27 @@
+//! Initialization code
+
+#![deny(warnings)]
+#![no_std]
+
+use panic_rtt_target as _; // panic handler
+
+pub use cortex_m_rt::entry;
+
+
+use microbit::hal::{self,pac};
+pub use pac::{p0, p1};
+pub use rtt_target::rprintln;
+
+
+#[inline(never)]
+pub fn init() -> (&'static p0::RegisterBlock, &'static p1::RegisterBlock) {
+    rtt_target::rtt_init_print!();
+    let device_periphs = pac::Peripherals::take().unwrap();
+    
+    // `display_pins!` initializes the display pins as outputs in push-pull mode
+    let port0 = hal::gpio::p0::Parts::new(device_periphs.P0);
+    let port1 = hal::gpio::p1::Parts::new(device_periphs.P1);
+    let _display_pins = microbit::display_pins!(port0, port1);
+
+    (unsafe { &*pac::P0::ptr() }, unsafe { &*pac::P1::ptr() })
+}
