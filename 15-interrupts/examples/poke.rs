99e025ae669ed8174a8ed99cd709625ceb3c2372
BartMassey <BartMassey@users.noreply.github.com>
2025-12-24 05:20:53 +0000

    deploy: 5466a0fd0c7360928bfbdeb132fc3f055e3ba1bb

---
+++ 15-interrupts/examples/poke.rs
@@ -0,0 +1,49 @@
+#![no_main]
+#![no_std]
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+use microbit::{
+    Board,
+    hal::{
+        gpiote,
+        pac::{self, interrupt},
+    },
+};
+
+/// This "function" will be called when an interrupt is received. For now, just
+/// report and panic.
+#[interrupt]
+fn GPIOTE() {
+    rprintln!("ouch");
+    panic!();
+}
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let button_a = board.buttons.button_a.into_floating_input();
+
+    // Set up the GPIOTE to generate an interrupt when Button A is pressed (GPIO
+    // wire goes low).
+    let gpiote = gpiote::Gpiote::new(board.GPIOTE);
+    let channel = gpiote.channel0();
+    channel
+        .input_pin(&button_a.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel.reset_events();
+
+    // Set up the NVIC to handle GPIO interrupts.
+    unsafe { pac::NVIC::unmask(pac::Interrupt::GPIOTE) };
+    pac::NVIC::unpend(pac::Interrupt::GPIOTE);
+
+    loop {
+        // "wait for interrupt": CPU goes to sleep until an interrupt.
+        asm::wfi();
+    }
+}
